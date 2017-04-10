c6c665e0e7fe1def8197db223e2e3297fadbde9a
Alexander Markelov <Alexander.Markelov@acronis.com>
2017-04-10 17:29:58 +0300

    added sys, dev mounts

--- src/fs.c
+++ src/fs.c
@@ -22,18 +22,25 @@ static char * produce_oldroot_path(const char * image_location) {
     return buf;
 }
 
-static char * produce_proc_path(const char * image_location) {
-    const char * proc_mount = "/proc";
-    char * buf = malloc(strlen(image_location) + strlen(proc_mount) + 10);
+static char * produce_system_path(const char * image_location, const char * type) {
+    char * buf = malloc(strlen(image_location) + strlen(type) + 10);
     buf[0] = 0;
     strcat(buf, image_location);
-    strcat(buf, proc_mount);
+    strcat(buf, "/");
+    strcat(buf, type);
     return buf;
 }
 
 void mount_system_to_image(const char *image_location) {
-    char * proc_location = produce_proc_path(image_location);
+    char * proc_location = produce_system_path(image_location, "proc");
+    char * sys_location = produce_system_path(image_location, "sys");
+    char * dev_location = produce_system_path(image_location, "dev");
     mount("proc", proc_location, "proc", 0, NULL);
+    mount("sys", sys_location, "sysfs", 0, NULL);
+    mount("/dev", dev_location, NULL, MS_BIND, NULL);
+    free(proc_location);
+    free(sys_location);
+    free(dev_location);
 }
 
 int mount_image(const char *image_location) {

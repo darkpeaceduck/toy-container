3e5dd098f68ae3a06eef8878aee9b58e8ac953f3
Alexander Markelov <Alexander.Markelov@acronis.com>
2017-04-10 12:16:28 +0300

    dir intead of file

--- src/journal.c
+++ src/journal.c
@@ -17,7 +17,7 @@ void journal_add_id(const char * journal_location, pid_t id) {
     journal_check_existiting(journal_location);
     char * buf = malloc(strlen(journal_location) + 100);
     buf[0] = 0;
-    sprintf(buf, "touch %s/%d", journal_location, id);
+    sprintf(buf, "mkdir %s/%d", journal_location, id);
     system(buf);
     free(buf);
 }
@@ -25,7 +25,7 @@ void journal_add_id(const char * journal_location, pid_t id) {
 void journal_remove_id(const char * journal_location, pid_t id) {
     char * buf = malloc(strlen(journal_location) + 100);
     buf[0] = 0;
-    sprintf(buf, "rm -f %s/%d", journal_location, id);
+    sprintf(buf, "rm -rf %s/%d", journal_location, id);
     system(buf);
     free(buf);
 }
